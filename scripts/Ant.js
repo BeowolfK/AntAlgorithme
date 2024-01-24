526c83b282f93c4b411acd6c2316a5c5558c3cab
Kenan Meylan <kenan@hosteur.com>
2024-01-24 16:41:47 +0100

    use of 2 display function

--- scripts/Ant.js
+++ scripts/Ant.js
@@ -1,71 +1,57 @@
 class Ant {
     constructor() {
-        this.speed = 1 ; 
-        this.possiblePositions = [
-            { x: 9, y: 10 },
-            { x: 8, y: 9 },
-            { x: 10, y: 9 },
-            { x: 9, y: 8 }
-        ];
-        this.position = this.getRandomPosition();
-        this.direction = 1; 
-        this.next_case = 0; 
+        this.speed = 1; 
+        this.position = { x: 9, y: 9 };
+        this.direction = 1;
+        this.next_case = 0;
         this.trajet = [];
         this.target = this.position;
     }
 
+    trajet_route() {
 
-    getRandomPosition() {
-        // Générer un index aléatoire
-        const randomIndex = Math.floor(Math.random() * this.possiblePositions.length);
-        // Retourner la position correspondante à l'index
-        return this.possiblePositions[randomIndex];
-    }
-    
-    trajet_route() {           
-       
         console.log("Current pos  : " + JSON.stringify(this.position))
-        this.trajet.push({...this.position});
+        this.trajet.push({ ...this.position });
         console.log("Trajet " + JSON.stringify(this.trajet));
     }
 
     next_etape(grid) {
         console.log("target : " + JSON.stringify(this.target) + " position : " + JSON.stringify(this.position));
-    
-        
+
+
         if (this.target.x == Math.floor(this.position.x) && this.target.y == Math.floor(this.position.y)) {
-        const directions = [
-            { dx: 0, dy: -1 }, // Haut
-            { dx: 0, dy: 1 },  // Bas
-            { dx: 1, dy: 0 },  // Droite
-            { dx: -1, dy: 0 }  // Gauche
-        ];
-    
-        // Enregistrez la case précédente
-        const previousPosition = { x: this.position.x, y: this.position.y };
-    
-        // Mélangez les directions de manière aléatoire
-        const shuffledDirections = directions.sort(() => Math.random() - 0.5);
-        
+            const directions = [
+                { dx: 0, dy: -1 }, // Haut
+                { dx: 0, dy: 1 },  // Bas
+                { dx: 1, dy: 0 },  // Droite
+                { dx: -1, dy: 0 }  // Gauche
+            ];
+
+            // Enregistrez la case précédente
+            const previousPosition = { x: this.position.x, y: this.position.y };
+
+            // Mélangez les directions de manière aléatoire
+            const shuffledDirections = directions.sort(() => Math.random() - 0.5);
+
             for (const dir of shuffledDirections) {
                 const nextX = Math.floor(this.position.x + dir.dx);
                 const nextY = Math.floor(this.position.y + dir.dy);
-        
+
                 // Vérifier si la prochaine case est à l'intérieur de la matrice
                 if (nextX >= 0 && nextX < grid.length && nextY >= 0 && nextY < grid[0].length) {
                     // Vérifier si la case est accessible et différente de la case précédente
-                    
-                //  if (grid[nextX][nextY].type !== "arbre" && grid[nextX][nextY].type !== "colonie" && (nextX !== previousPosition.x || nextY !== previousPosition.y)) {
-                    if ((grid[nextY][nextX] instanceof Herbe || grid[nextY][nextX] instanceof Nourriture) && (nextX !== previousPosition.x || nextY !== previousPosition.y)) {
-                    
-                        console.log("Next grid : " +  JSON.stringify(grid[nextY][nextX]));
+
+                    //  if (grid[nextX][nextY].type !== "arbre" && grid[nextX][nextY].type !== "colonie" && (nextX !== previousPosition.x || nextY !== previousPosition.y)) {
+                    if ((grid[nextY][nextX].type === "herbe" || grid[nextY][nextX].type === "nourriture") && (nextX !== previousPosition.x || nextY !== previousPosition.y)) {
+
+                        console.log("Next grid : " + JSON.stringify(grid[nextY][nextX]));
                         // Enregistrez la nouvelle case comme la case précédente
-                        
+
                         previousPosition.x = Math.floor(this.position.x);
                         previousPosition.y = Math.floor(this.position.y);
-                        this.target = {x: nextX, y : nextY}; 
+                        this.target = { x: nextX, y: nextY };
                         // Retourner la direction de la prochaine case valide
-                        this.direction = dir; 
+                        this.direction = dir;
                         return dir;
                     }
                 }
@@ -76,13 +62,11 @@ class Ant {
             const nextY = Math.floor(this.position.y + this.direction.dy);
 
             if (nextX >= 0 && nextX < grid.length && nextY >= 0 && nextY < grid[0].length) {
-                if (grid[nextY][nextX] instanceof Herbe || grid[nextY][nextX] instanceof Nourriture) {
+                if (grid[nextY][nextX].type === "herbe" || grid[nextY][nextX].type === "nourriture") {
 
-                    return this.direction; 
+                    return this.direction;
                 }
-
             }
-
         }
         // Aucune direction valide trouvée
     }
--- scripts/app.js
+++ scripts/app.js
@@ -65,21 +65,22 @@ class Model {
             [new Arbre(), new Herbe(), new Arbre(), new Herbe(), new Arbre(), new Herbe(), new Arbre(), new Arbre(), new Herbe(), new Arbre(), new Herbe(), new Arbre(), new Herbe(), new Herbe(), new Nourriture(), new Herbe(), new Herbe(), new Arbre()],
             [new Arbre(), new Herbe(), new Herbe(), new Herbe(), new Arbre(), new Nourriture(), new Arbre(), new Arbre(), new Herbe(), new Herbe(), new Herbe(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre()],
             [new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre(), new Arbre()]
-        
+
         ];
         this.time = "00:00"; // Secondes:Microsecondes
-        
+        this.fourmis = [];
+
         // Mouvement
-        this._startTime     = Date.now();
-        this._lag           = 0;
-        this._fps           = 60; // Frame rate.
+        this._startTime = Date.now();
+        this._lag = 0;
+        this._fps = 60; // Frame rate.
         this._frameDuration = 1000 / this._fps;
-        this._position      = {x: 9, y:10};
-        this._cellSize      = 100; // La taille d'une cellule en pixel.
-        this._speed         = 10; // Nous voulons que 1 cellule (de notre grille) soit parcourue en 1 seconde (doit être dépendant des FPS fixés car la fonction est appelée à chaque frame). Notre unité de vitesse est donc "le nombre de cellules de la grille parcourues/seconde".
-        this._direction     = 1; // En radian.
-        this._timer         = 0;
-        this._block         = true;  
+        this._position = { x: 9, y: 10 };
+        this._cellSize = 100; // La taille d'une cellule en pixel.
+        this._speed = 10; // Nous voulons que 1 cellule (de notre grille) soit parcourue en 1 seconde (doit être dépendant des FPS fixés car la fonction est appelée à chaque frame). Notre unité de vitesse est donc "le nombre de cellules de la grille parcourues/seconde".
+        this._direction = 1; // En radian.
+        this._timer = 0;
+        this._block = true;
     }
 
     // Bindings pour l'affichage
@@ -89,11 +90,11 @@ class Model {
 
     // Fonction qui lance le jeu
     play() {
-        
+
         this.generate_ant();
 
         this.display(this.grid, this.ant1.position);
-        
+
         this.update();
         console.log('Final road : ' + JSON.stringify(this.ant1.trajet))
     }
@@ -101,6 +102,10 @@ class Model {
     // Bindings pour le temps 
     bindDisplayTime(callback) {
         this.displayTime = callback;
+    }
+
+    bindDisplayAnt(callback) {
+        this.displayAnt = callback;
     }
 
     // Fonction qui remet a zero toutes les cases
@@ -133,7 +138,7 @@ class Model {
 
     // Fonction qui lance le temps et démarrer la clock
     getTime() {
-        this.counter = setInterval(() => { 
+        this.counter = setInterval(() => {
             let time = this.time.split(":");
             let seconds = parseInt(time[0]);
             let microseconds = parseInt(time[1]);
@@ -179,12 +184,13 @@ class Model {
 
     generate_ant() {
         this.ant1 = new Ant();
-        this.ant1.trajet_route(); 
-      
+        this.fourmis.push(this.ant1);
+        this.ant1.trajet_route();
+
         //this.ant1.next_etape(this.grid); 
     }
 
-    move = function(durationFrame) {
+    move(durationFrame) {
         /*
             Calculer le vecteur direction:
             https://reglecompas.fr/wp-content/uploads/2020/10/coord-trigo.png
@@ -197,12 +203,12 @@ class Model {
             const { dx, dy } = nextDirection;
             // Utilisez dx et dy comme vous le souhaitez
             console.log(`Prochaine direction : dx = ${dx}, dy = ${dy}`);
-             /* Multiplier la direction par la vitesse */
+            /* Multiplier la direction par la vitesse */
             //this.ant1.position.x += dx * this._speed / this._fps; // On divise par les fps car la fonction est appelée selon un fps donné (#cellGrid/seconde).
             //this.ant1.position.y += dy * this._speed / this._fps;
-            console.log("Next case : "+ this.ant1.position.x + dx )
-            this.ant1.position.x += dx * this._speed / this._fps; 
-            this.ant1.position.y += dy * this._speed / this._fps; 
+            console.log("Next case : " + this.ant1.position.x + dx)
+            this.ant1.position.x += dx * this._speed / this._fps;
+            this.ant1.position.y += dy * this._speed / this._fps;
             //this.ant1.position.x += dx; 
             // this.ant1.position.y += dy; 
             this.ant1.trajet_route()
@@ -214,49 +220,50 @@ class Model {
         }
 
 
-       
-        }
 
-    
-    update = function() {
+    }
+
+
+    update() {
         /* Calcul du deltaTime */
-        
+
         let currentTime = Date.now();
-        let deltaTime   = currentTime - this._startTime; // La durée entre deux appels (entre 2 frames).
+        let deltaTime = currentTime - this._startTime; // La durée entre deux appels (entre 2 frames).
         this._lag += deltaTime;
         this._startTime = currentTime;
         this._timer += deltaTime;
-        
+
         /* Mettre à jour la logique si la variable _lag est supérieure ou égale à la durée d'une frame */
         console.log("_lag + _frameDuration" + this._lag + " " + this._frameDuration)
         while (this._lag >= this._frameDuration) {
-           
+
             /* Mise à jour de la logique et de la vue */
-            
+
             this.move(this._frameDuration);
-            this.display(this.grid,this.ant1.position);
+            this.display(this.grid);
+            this.displayAnt(this.ant1.position);
             /* Réduire la variable _lag par la durée d'une frame */
             this._lag -= this._frameDuration;
 
         }
         console.log("check_pos : " + JSON.stringify(this.ant1.position.x))
-        
+
         if (this._block == true) {
             console.log("ici");
             requestAnimationFrame(this.update.bind(this)); // La fonction de rappel est généralement appelée 60 fois par seconde.
-            
-            
+
+
             if (this.ant1.trajet.length >= 10) {
                 const lastTenPositions = this.ant1.trajet.slice(-10);
                 if (lastTenPositions.every(pos => pos.x === lastTenPositions[0].x && pos.y === lastTenPositions[0].y)) {
                     console.log("Les 10 dernières positions sont identiques. Arrêt du processus.");
-                    this._block = false; 
+                    this._block = false;
                 }
             }
         }
 
         console.log(this.ant1.position, this._timer / 1000);
-        }
+    }
 
 
 }
@@ -274,8 +281,8 @@ class View {
         this.toggleClock();
         this.togglePheromone();
     }
-    
-    display(grid, position){
+
+    display(grid) {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         let nbLines = grid.length;
@@ -294,13 +301,13 @@ class View {
                         let randX = 4 + i % 4;
                         let randY = j % 4;
                         this.ctx.drawImage(GRASS_IMAGE, randX * 32, randY * 32, 32, 32, j * this.cellSize, i * this.cellSize, this.cellSize, this.cellSize);
-
-                        let proportional = Math.log10(tile.pheromone * (10 - 1) + 1) * ((this.cellSize - 5) / 2) ;
+                        
+                        let proportional = Math.log10(tile.pheromone * (10 - 1) + 1) * ((this.cellSize - 5) / 2);
 
                         if (this.statusPheromone) {
                             this.ctx.font = "bold 14px Arial";
                             this.ctx.fillStyle = this.colorChooser(proportional);
-                            this.ctx.fillText(tile.pheromone, j * this.cellSize + 15, i * this.cellSize + this.cellSize / 2  + 6);
+                            this.ctx.fillText(tile.pheromone, j * this.cellSize + 15, i * this.cellSize + this.cellSize / 2 + 6);
                         } else {
                             this.ctx.beginPath();
                             this.ctx.arc(j * this.cellSize + this.cellSize / 2, i * this.cellSize + this.cellSize / 2, proportional, 0, 2 * Math.PI);
@@ -324,10 +331,10 @@ class View {
                 }
             }
         }
-        console.log("Print : " + position.x + " " + position.y)
-        this.ctx.drawImage(ANT_IMAGE, position.x * this.cellSize , position.y * this.cellSize , this.cellSize /2 , this.cellSize /2 );
-        // this.ctx.fillRect(position.x * this.cellSize, position.y * this.cellSize, 25, 25);
-        console.log("pos 240 " + position.x + " " + position.y)
+    }
+
+    displayAnt(position) {
+        this.ctx.drawImage(ANT_IMAGE, position.x * this.cellSize, position.y * this.cellSize, this.cellSize / 2, this.cellSize / 2);
     }
 
     // Bindings pour le temps
@@ -379,7 +386,7 @@ class View {
     // Fonction qui choisi la couleur en fonction de la valeur des pheromones
     colorChooser(value) {
         let color = "";
-        if(value > 20){
+        if (value > 20) {
             color = "#7a0014";
         } else if (value > 18) {
             color = "purple";
@@ -391,13 +398,13 @@ class View {
             color = "#535ce0";
         } else if (value > 10) {
             color = "#727af2";
-        }else if (value > 8) {
+        } else if (value > 8) {
             color = "#858cf2";
-        }else if (value > 6) {
+        } else if (value > 6) {
             color = "#a5aafa";
-        }else if (value > 4) {
+        } else if (value > 4) {
             color = "#c8cbfa";
-        }else {
+        } else {
             color = "#ffffff";
         }
         return color;
@@ -416,19 +423,26 @@ class Controller {
 
         this.bindDisplayTime = this.bindDisplayTime.bind(this);
         this.model.bindDisplayTime(this.bindDisplayTime);
-        
+
         this.bindGetTime = this.bindGetTime.bind(this);
         this.view.bindGetTime(this.bindGetTime);
-        
+
         this.bindResetTime = this.bindResetTime.bind(this);
         this.view.bindResetTime(this.bindResetTime);
-        
+
+        this.bindDisplayAnt = this.bindDisplayAnt.bind(this);
+        this.model.bindDisplayAnt(this.bindDisplayAnt);
+
         this.model.bindDisplay(this.bindDisplay.bind(this))
         this.model.play()
     }
 
-    bindDisplay(grid, position) {
-        this.view.display(grid,position)
+    bindDisplay(grid) {
+        this.view.display(grid)
+    }
+
+    bindDisplayAnt(position) {
+        this.view.displayAnt(position)
     }
 
     bindDisplayTime(time) {
@@ -462,19 +476,19 @@ const SHADOW_IMAGE = new Image();
 SHADOW_IMAGE.src = 'assets/shadow.png';
 
 Promise.all([
-    new Promise((resolve) => { 
-        HEXTILES_IMAGE.addEventListener('load', () => { resolve(); }); 
+    new Promise((resolve) => {
+        HEXTILES_IMAGE.addEventListener('load', () => { resolve(); });
     }),
-    new Promise((resolve) => { 
+    new Promise((resolve) => {
         TREE_IMAGE.addEventListener('load', () => { resolve(); });
     }),
-    new Promise((resolve) => { 
+    new Promise((resolve) => {
         ANT_IMAGE.addEventListener('load', () => { resolve(); });
     }),
-    new Promise((resolve) => { 
+    new Promise((resolve) => {
         GRASS_IMAGE.addEventListener('load', () => { resolve(); });
     }),
-    new Promise((resolve) => { 
+    new Promise((resolve) => {
         SHADOW_IMAGE.addEventListener('load', () => { resolve(); });
     })
 ]).then(() => {
